01fa3b0c80d24e93e25109e02e85f40fda3a6f8d
Zach Moore <Zach.Moore@schneider-electric.com>
2019-01-23 17:00:36 -0800

    Added step in the build script to unzip libgstlibav.dll if it doesn't exist.

--- build.cmd
+++ build.cmd
@@ -20,6 +20,11 @@ if NOT DEFINED CMAKE_EXE (
   set CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe"
 )
 
+REM Unzip libgstlibav.dll if it doesn't exist
+if not exist "%CD%\VideoXpertSdkMedia\ThirdParty\GStreamer\1.14.4\x64\gstreamer_runtime\lib\gstreamer-1.0\libgstlibav.dll" (
+powershell  -Command "Expand-Archive '%CD%\VideoXpertSdkMedia\ThirdParty\GStreamer\1.14.4\x64\gstreamer_runtime\lib\gstreamer-1.0\libgstlibav.zip' -DestinationPath '%CD%\VideoXpertSdkMedia\ThirdParty\GStreamer\1.14.4\x64\gstreamer_runtime\lib\gstreamer-1.0\'"
+)
+
 REM Create the output path for the NuGet package if it doesn't exist
 if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
 
