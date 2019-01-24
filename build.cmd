9c3a6a44659aaff10b8ede4bcd05916440b84315
Zach Moore <Zach.Moore@schneider-electric.com>
2019-01-23 16:36:40 -0800

    Updated build script.

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-if NOT DEFINED BUILD_NUMBER (
+if NOT DEFINED MAJOR_VER (
   set MAJOR_VER=3
 )
 
-if NOT DEFINED BUILD_NUMBER (
+if NOT DEFINED MINOR_VER (
   set MINOR_VER=0
 )
 
@@ -13,16 +13,21 @@ if NOT DEFINED BUILD_NUMBER (
 )
 
 if NOT DEFINED OUTPUT_PATH (
-  set OUTPUT_PATH=%CD%
+  set OUTPUT_PATH="%CD%\output"
 )
 
 if NOT DEFINED CMAKE_EXE (
   set CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe"
 )
 
+REM Create the output path for the NuGet package if it doesn't exist
+if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
+
+REM Install NuGet dependencies
 pushd VideoXpertSdkMedia
 call ..\Nuget\nuget.exe install packages.config -OutputDirectory packages -ExcludeVersion
 
+REM Setup VideoXpertSdkMedia build
 if exist build\ rd /q /s build
 mkdir build & pushd build
 
@@ -33,12 +38,12 @@ popd
 mkdir VxSdkMedia_x64 & pushd VxSdkMedia_x64
 %CMAKE_EXE% -G "Visual Studio 14 Win64" ..\..\
 popd
-
 popd
 
+REM Build VideoXpertSdkMedia
 %CMAKE_EXE% --build build\VxSdkMedia_Win32 --config Release
 %CMAKE_EXE% --build build\VxSdkMedia_x64 --config Release
-
 popd
 
+REM Create Nuget Package
 call Nuget\nuget.exe pack Nuget\VideoXpertSdk-Media.nuspec -Version %MAJOR_VER%.%MINOR_VER%.%BUILD_NUMBER% -OutputDirectory %OUTPUT_PATH%
